205cbbcd52224e01a87d46f9a2eb07ff25baa8aa
David Vicent <david@sappitotech.com>
2019-04-05 15:58:40 -0400

    añadido react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,94 +1,37 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
 
-import { firebaseDatabase } from "./initializers/firebase";
+// import { firebaseDatabase } from "./initializers/firebase";
 import "./app.css";
 
-import Layout from "./components/Layout";
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			information: null,
-			title: "",
-			description: "",
-			isLoading: false
-		};
+	//form
+	// handleTitleInput(e) {
+	// 	this.setState({ title: e.target.value });
+	// }
 
-		this.handleTitleInput = this.handleTitleInput.bind(this);
-		this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	componentWillMount() {
-		this.handleInformation();
-	}
+	// handleDescriptionInput(e) {
+	// 	this.setState({ description: e.target.value });
+	// }
 
-	handleInformation() {
-		this.setState({ isLoading: true });
-		firebaseDatabase()
-			.ref("information")
-			.on("value", snapshot => {
-				this.setState({
-					information: snapshot.val(),
-					isLoading: false
-				});
-			});
-	}
-	handleTitleInput(e) {
-		this.setState({ title: e.target.value });
-	}
-
-	handleDescriptionInput(e) {
-		this.setState({ description: e.target.value });
-	}
-
-	handleSubmit(e) {
-		e.preventDefault();
-		firebaseDatabase()
-			.ref("information")
-			.set({
-				title: this.state.title,
-				description: this.state.description
-			});
-	}
-
-	renderText() {
-		if (this.state.isLoading) {
-			return <h1>Cargando</h1>;
-		} else {
-			return (
-				<div>
-					<h1>{this.state.information.title}</h1>
-					<p>{this.state.information.description}</p>
-				</div>
-			);
-		}
-	}
+	// handleSubmit(e) {
+	// 	e.preventDefault();
+	// 	firebaseDatabase()
+	// 		.ref("information")
+	// 		.set({
+	// 			title: this.state.title,
+	// 			description: this.state.description
+	// 		});
+	// }
 
 	render() {
 		return (
-			<Layout>
-				<div className="row">
-					<div className="card mx-auto card-orange mt-5">
-						{this.renderText()}
-					</div>
-
-					{/* <form onSubmit={this.handleSubmit}>
-            <input
-              className="form-control"
-              type="text"
-              placeholder="Title"
-              onChange={this.handleTitleInput}
-            />
-            <input
-              type="text"
-              className="form-control"
-              placeholder="description"
-              onChange={this.handleDescriptionInput}
-            />
-            <input type="submit" value="save" />
-          </form> */}
-				</div>
-			</Layout>
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+				</Switch>
+			</Router>
 		);
 	}
 }
---
+++ src/components/pages/Home.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import Layout from "../Layout";
+import { firebaseDatabase } from "../../initializers/firebase";
+
+/*
+ <form onSubmit={this.handleSubmit}>
+    <input
+        className="form-control"
+        type="text"
+        placeholder="Title"
+        onChange={this.handleTitleInput}
+    />
+    <input
+        type="text"
+        className="form-control"
+        placeholder="description"
+        onChange={this.handleDescriptionInput}
+    />
+    <input type="submit" value="save" />
+</form> 
+*/
+
+class Home extends Component {
+	constructor() {
+		super();
+		this.state = {
+			information: null,
+			isLoading: false
+		};
+	}
+	componentWillMount() {
+		this.handleInformation();
+	}
+
+	handleInformation() {
+		this.setState({ isLoading: true });
+		firebaseDatabase()
+			.ref("information")
+			.on("value", snapshot => {
+				this.setState({
+					information: snapshot.val(),
+					isLoading: false
+				});
+			});
+	}
+	renderText() {
+		if (this.state.isLoading) {
+			return <h1>Cargando</h1>;
+		} else {
+			return (
+				<div>
+					<h1>{this.state.information.title}</h1>
+					<p>{this.state.information.description}</p>
+				</div>
+			);
+		}
+	}
+	render() {
+		return (
+			<Layout>
+				<div className="row">
+					<div className="card mx-auto card-orange mt-5">
+						{this.renderText()}
+					</div>
+				</div>
+			</Layout>
+		);
+	}
+}
+export default Home;

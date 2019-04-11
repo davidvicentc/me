80d4f39042c095ff70352dc53efed6ebff002ace
David Vicent <david@sappitotech.com>
2019-04-11 14:47:02 -0400

    Updates

---
+++ precache-manifest.0f1c120f162bff5ea8ad3db9897acc46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff8507be9a025716d359",
+    "url": "/dvicent/static/css/main.b72cbf8b.chunk.css"
+  },
+  {
+    "revision": "ff8507be9a025716d359",
+    "url": "/dvicent/static/js/main.aaa72e7b.chunk.js"
+  },
+  {
+    "revision": "81d0ecd44062c63f33e6",
+    "url": "/dvicent/static/js/runtime~main.928fc5cd.js"
+  },
+  {
+    "revision": "76837e953b1da75d0bee",
+    "url": "/dvicent/static/js/2.93ca7daf.chunk.js"
+  },
+  {
+    "revision": "7bcfd9d6b1a40587f968040c1e8330f2",
+    "url": "/dvicent/index.html"
+  }
+];
